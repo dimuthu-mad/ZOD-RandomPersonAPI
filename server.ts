037208f827d3cf20c0cc6ad42a3cce2f55a62ef9
dimuthu-mad <dimuthumaduranga96@gmail.com>
2026-03-12 11:13:18 +0100

    create random person GET

--- server.ts
+++ server.ts
@@ -1,14 +1,44 @@
 import express from "express";
-import { z } from "zod";
+import { email, z } from "zod";
+import { da } from "zod/locales";
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("RandomPersonAPI is running");
+  res.send("pong");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+const personSchema = z.object({
+  results: z.array(
+    z.object({
+      name: z.object({
+        first: z.string(),
+        last: z.string(),
+      }),
+      gender: z.string(),
+      location: z.object({
+        city: z.string(),
+        country: z.string(),
+      }),
+      email: z.email(),
+    }),
+  ),
+});
+
+app.get("/random-person", async (req, res) => {
+  const response = await fetch("https://randomuser.me/api/");
+  const data = await response.json();
+
+  const validatedPerson = personSchema.safeParse(data);
+  if (validatedPerson.success) {
+    res.json(validatedPerson.data);
+  } else {
+    res.status(500).json({ error: "Failed to fetch random person" });
+  }
 });
